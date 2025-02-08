58b67f6146943dc10e4c795cf0493c73f5ce9c1c
22Vityk <Vityk2904@gmail.com>
2025-02-08 19:31:58 +0200

    Home work tree update

--- Home3/main.js
+++ Home3/main.js
@@ -121,6 +121,7 @@
 //
 // </div>`)
 // }
+
 // ШАБЛОН
 // <div class="product-card">
 //     <h3 class="product-title">TITLE. Price - PRICE</h3>
@@ -131,19 +132,19 @@
 // --------------------
 
 // є масив
-let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
-];
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
 
 // let x= 0
 // while (x < users.length) {
@@ -169,6 +170,7 @@ let users = [
 //         document.write(`<div>${user.status} ${user.name}</div>`)
 //     }
 // }
+
 // let x = 0
 // while (x < users.length) {
 //     let user = users[x];
@@ -192,9 +194,6 @@ let users = [
 //         console.log(old )
 //     }
 // }
-
-
-
 
 // за допомоги циклу вивести:
 //     - користувачів зі статусом true
